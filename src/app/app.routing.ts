bd8e9d57494380008df656ffa082dc0e6450f02c
Aaron Ross and Ryan Ferris <aaron+ryan@>
2018-02-28 10:04:26 -0800

    Welcome Component
    Update the Welcome, and add routes to login as either an Admin, or a
    User.

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,11 +1,21 @@
 import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { WelcomeComponent } from './welcome/welcome.component';
+import { ProjectListComponent } from './project-list/project-list.component';
+import { LoginComponent } from './login/login.component';
 
 const appRoutes: Routes = [
 	{
 		path: '',
 		component: WelcomeComponent
+	},
+	{
+		path: 'admin',
+		component: ProjectListComponent
+	},
+	{
+		path: 'user',
+		component: LoginComponent
 	}
 ];
 
