f285ae5e63e758a6d403345aa6d10ebd270e07be
Aaron Ross and Ryan Ferris <aaron+ryan@>
2018-02-28 11:17:30 -0800

    Model
    Create the User and Project Class Models.

--- src/app/models/project.model.ts
+++ src/app/models/project.model.ts
@@ -1,2 +1,9 @@
 export class Project {
+
+	public raised: number;
+	public funded: boolean;
+	public contributors: string[];
+
+	constructor(public title: string, public type: string, public description: string, public swag: string, public goal: number, public admin: string) { }
+
 }
--- src/app/models/user.model.ts
+++ src/app/models/user.model.ts
@@ -1,2 +1,8 @@
 export class User {
+
+	public favorites: string[];
+	public donations: object[];
+
+	constructor(public username: string, public email: string, public password: string) { }
+
 }
