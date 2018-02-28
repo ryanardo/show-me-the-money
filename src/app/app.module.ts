870d324ae1fd1209daa0ec21e0d51a4b0148cead
Aaron Ross and Ryan Ferris <aaron+ryan@>
2018-02-28 09:54:00 -0800

    App Module
    Update the app.doule.ts file with the requesite Agnular components.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { FavoritesComponent } from './favorites/favorites.component';
+import { ProjectListComponent } from './project-list/project-list.component';
+import { ProjectDetailComponent } from './project-detail/project-detail.component';
+import { LoginComponent } from './login/login.component';
+import { FeatureComponent } from './feature/feature.component';
+import { UserDetailComponent } from './user-detail/user-detail.component';
 
 @NgModule({
 	declarations: [
-		AppComponent
+		AppComponent,
+		WelcomeComponent,
+		FavoritesComponent,
+		ProjectListComponent,
+		ProjectDetailComponent,
+		LoginComponent,
+		FeatureComponent,
+		UserDetailComponent
 	],
 	imports: [
 		NgModule,
